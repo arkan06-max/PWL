8f2f9cb2acf7b0dcd8dbbe2e6b8ac27d6c5e3edc
arkan06-max <arkanyumna021@gmail.com>
2025-02-25 21:19:08 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="X-UA-Compatible" content="IE=edge">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <form acction="simpan.php" method="post">
+        NIM : <input type="text" name="nim" id=""><br>
+        Nama : <input type="text" name="nama" id=""><br>
+        
+        <button type="submit" >Simpan</button>
+    </form>
+</body>
+</html>
+   
